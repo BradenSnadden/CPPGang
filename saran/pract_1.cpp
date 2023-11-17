f8a08023d1a93875026f5908e84ee83c5b941783
saranpat pisanuwong <saranpat8696@gmail.com>
2023-11-17 16:17:23 +1030

    damn

---
+++ saran/pract_1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main()
+{
+    int a = 3;
+    int b = 4;
+    int sum = a + b;
+    int p = a * b;
+    std::cout << "The sum is: " << sum << " The product is: " << p << std::endl;
+
+}
